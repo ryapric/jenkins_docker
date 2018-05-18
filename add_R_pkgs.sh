2cbdef5ab9b597550598f3b3015627ca24310b4a
Ryan Price <rprice@aggio.io>
2018-05-18 13:04:24 -0500

    Add container name check to R package installer.

--- add_R_pkgs.sh
+++ add_R_pkgs.sh
@@ -1,7 +1,13 @@
 # /usr/bin/env sh
 set -e
 
-docker container exec jenkins \
+# Check for container name passed to script
+if [ -z "$1" ]; then
+    printf "You must provide the name of the running Jenkins container, via 'sh add_R_pkgs.sh {jenkins_container_name}'\n"
+    exit 1
+fi
+
+docker container exec "$1" \
     Rscript -e " \
         install.packages(c( \
             'tidyverse', \
